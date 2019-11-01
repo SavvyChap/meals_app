72f49a6f266edaff20e3ecfc60b773f5a35f45f5
August Money <august@planetexpress.io>
2019-10-31 23:44:07 -0500

    Added meal.dart file which created a model for Meal.

--- lib/catagory_meals_screen.dart
+++ lib/catagory_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy-data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
 //  final String categoryId;
@@ -12,13 +14,16 @@ class CategoryMealsScreen extends StatelessWidget {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('Recipes for this category'),
-      ),
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        return Text(categoryMeals[index].title);
+      }, itemCount: categoryMeals.length,),
     );
   }
 }
