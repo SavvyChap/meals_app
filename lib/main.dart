f5ad1eddd7045d3f5481364d2f8e14d54bc2c14c
August Money <august@planetexpress.io>
2019-10-31 23:19:13 -0500

    Initial setup with main screen of categories and screen push for when you select a category.

--- lib/main.dart
+++ lib/main.dart
@@ -29,9 +29,11 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: CategoriesScreen(),
+//      home: CategoriesScreen(),
+      initialRoute: '/', // good strategy to include auth
       routes: {
-        '/category-meals': (ctx) => CategoryMealsScreen(),
+        '/': (ctx) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
       },
     );
   }
