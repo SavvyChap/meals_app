cb8a603734db9d9ccdafb2b93f51619c17acd46f
August Money <august@planetexpress.io>
2019-11-02 16:50:57 -0500

    Made some navigation changes in regards to pushing a popping screens.

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/drawer_main.dart';
 
 class FilterScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -6,6 +7,10 @@ class FilterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Filter Selection'),
+      ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Filters Screen'),
       ),
